e30070a1dac06fea08a5f3244be9278cb3cd4915
XuanDatNguyen <nguyenxuandat.bqp@gmail.com>
2022-03-11 00:03:52 +0700

    ok

---
+++ database/migrations/2022_03_10_212746_create_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 200);
+            $table->string('slug',200);
+            $table->longText('description')->nullable();
+            $table->integer('group_id')->unsigned();
+            $table->foreign('group_id')->references('id')->on('groups')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('types');
+    }
+}
